a03d88869f6a7fb116ad0bb3f3f5574f2834592f
sandey_lee <sandey_lee@yahoo.com.tw>
2017-11-08 10:09:40 +0800

    homework_2_H1191
    Homework_2 H1191 2017.11.07

---
+++ homework_2_H1191.py
@@ -0,0 +1,17 @@
+Python 3.7.0a2 (v3.7.0a2:f7ac4fe52a, Oct 16 2017, 21:11:18) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+while True:
+	list = []
+	list = [input('a:'),input('b:')]
+	a,b = list
+	c=int(a) + int(b)
+	print(c)
+	break
+
+a:3
+b:6
+9
+>>> 
